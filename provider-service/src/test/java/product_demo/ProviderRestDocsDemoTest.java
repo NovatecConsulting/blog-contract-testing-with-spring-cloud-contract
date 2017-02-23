a8cb869f0030457a17dcee8986cc8c69d172298b
Antoniya Atanasova <antoniya.atanasova@novatec-gmbh.de>
2017-02-23 13:26:44 +0100

    Refactoring.

--- provider-service/src/test/java/product_demo/ProviderRestDocsDemoTest.java
+++ provider-service/src/test/java/product_demo/ProviderRestDocsDemoTest.java
@@ -1,17 +1,20 @@
 package product_demo;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.restdocs.WireMockRestDocs;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -22,17 +25,37 @@ public class ProviderRestDocsDemoTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /**
+     * The Query parameters are not generated in the restdocs-resource.json
+     * To generate them you need to use WireMockRestDocs - see restDocsWireMockDemoTest
+     */
     @Test
     public void restDocsDemoTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/product")
-                .param("id", "537"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content()
+        mockMvc.perform(get("/product?id=537"))
+                .andExpect(status().isOk())
+                .andExpect(content()
                         .json("{" +
                                 "\"name\": \"Consumer Test\"," +
                                 "\"description\" : \"Consumer Test verifies provider\"," +
                                 "\"type\": \"testing product\"" +
                                 "}"))
                 .andDo(document("restdocs-resource"));
+    }
+
+    @Test
+    public void restDocsWireMockDemoTest() throws Exception {
+        mockMvc.perform(get("/product?id=537"))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("{" +
+                                "\"name\": \"Consumer Test\"," +
+                                "\"description\" : \"Consumer Test verifies provider\"," +
+                                "\"type\": \"testing product\"" +
+                                "}"))
+                .andDo(WireMockRestDocs.verify().
+                        wiremock(WireMock
+                                .get(WireMock.urlPathEqualTo("/product"))
+                                .withQueryParam("id", equalTo("537")))
+                        .stub("restdocs-resource-2"));
     }
 }
